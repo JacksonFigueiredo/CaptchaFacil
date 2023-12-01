2223651e1db386bb6d858e8084d54eae91183dd3
Jackson Figueiredo <ext.j.figueiredo@farfetch.com>
2023-12-01 16:42:25 -0300

    Enviando Primeiro Commit

---
+++ CaptchaFacil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+
+namespace CaptchaFacil
+{
+    public class CaptchaFacil
+    {
+        public class CaptchaImage
+        {
+            public string Text { get; private set; }
+
+            public CaptchaImage()
+            {
+                Text = GenerateRandomText(6);
+            }
+
+            private string GenerateRandomText(int length)
+            {
+                var random = new Random();
+                var text = new string[6];
+                for (int i = 0; i < length; i++)
+                {
+                    text[i] = ((char)(random.Next(26) + 65)).ToString();
+                }
+                return string.Concat(text);
+            }
+
+            private void AddNoise(Bitmap bitmap, int width, int height)
+            {
+                var random = new Random();
+
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int x1 = random.Next(width);
+                        int y1 = random.Next(height);
+                        int x2 = random.Next(width);
+                        int y2 = random.Next(height);
+                        g.DrawLine(new Pen(Color.Gray, 2), x1, y1, x2, y2);
+                    }
+
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        int x = random.Next(width);
+                        int y = random.Next(height);
+                        bitmap.SetPixel(x, y, Color.FromArgb(random.Next(255), random.Next(255), random.Next(255)));
+                    }
+                }
+            }
+
+            private Bitmap DeformImage(Bitmap originalImage)
+            {
+                Bitmap deformedImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+                using (Graphics g = Graphics.FromImage(deformedImage))
+                {
+                    var matrix = new System.Drawing.Drawing2D.Matrix();
+                    var random = new Random();
+                    float skewAmount = random.Next(-10, 10) / 10.0f;
+                    matrix.Shear(skewAmount, 0);
+                    g.Transform = matrix;
+
+                    g.DrawImage(originalImage, new Point(0, 0));
+                }
+
+                return deformedImage;
+            }
+
+
+
+            public byte[] GenerateImage()
+            {
+                using (Bitmap bitmap = new Bitmap(200, 50))
+                {
+                    using (Graphics g = Graphics.FromImage(bitmap))
+                    {
+                        g.Clear(Color.White);
+                        g.DrawString(Text, new Font("Arial", 25, FontStyle.Bold), Brushes.Gray, new PointF(10, 10));
+
+                        AddNoise(bitmap, bitmap.Width, bitmap.Height);
+                    }
+
+                    using (Bitmap deformedBitmap = DeformImage(bitmap))
+                    {
+                        using (var ms = new MemoryStream())
+                        {
+                            deformedBitmap.Save(ms, ImageFormat.Png);
+                            return ms.ToArray();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,61 @@
+CaptchaFacil Generator
+Overview
+
+CaptchaFacil is a powerful and user-friendly Captcha image generator designed for .NET applications. It provides a straightforward way to add an extra layer of security to your forms, making it more challenging for automated bots to interact with your application.
+Getting Started
+
+To begin using CaptchaFail in your project, follow these instructions:
+
+    Instantiate Captcha:
+        Include the CaptchaFacil library in your project.
+        Create an instance of the CaptchaImage class within your form to generate a Captcha image.
+
+    Display Captcha:
+        Implement the GenerateCaptcha method in your form. This method will generate a new Captcha, convert it into a format that can be displayed, and update your form to show the Captcha image.
+
+    Code Example
+
+    Below is a code example showcasing how to integrate CaptchaFacil into a Windows Forms application:
+
+public class Form1 : Form
+{
+    private CaptchaImage captcha;
+
+    public Form1()
+    {
+        InitializeComponent();
+        GenerateCaptcha();
+    }
+
+    private void GenerateCaptcha()
+    {
+        captcha = new CaptchaImage();
+        using (var ms = new System.IO.MemoryStream(captcha.GenerateImage()))
+        {
+            pictureBox1.Image = Image.FromStream(ms);
+        }
+        label1.Text = ""; // Clears any previous message
+    }
+}
+
+In this example, pictureBox1 is a PictureBox control to display the Captcha, and label1 is a Label for messages.
+
+Validate Captcha:
+
+    Include a method to validate the Captcha input by the user.
+    Here is an example method ValidateCaptcha:
+
+private void ValidateCaptcha()
+{
+    if (textBox1.Text.Equals(captcha.Text, StringComparison.OrdinalIgnoreCase))
+    {
+        label1.Text = "CAPTCHA correct!";
+        label1.ForeColor = Color.Green;
+        // Additional logic can be added here, like proceeding to the next step
+    }
+    else
+    {
+        label1.Text = "Incorrect CAPTCHA, please try again.";
+        label1.ForeColor = Color.Red;
+    }
+}
